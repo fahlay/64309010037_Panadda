689d3c9af2bdfe07e5205af3f5e8ede13b41131b
fahlay <116155807+fahlay@users.noreply.github.com>
2022-11-22 09:45:58 +0700

    Create reviewJS3.js

---
+++ Week5/reviewJS3.js
@@ -0,0 +1,8 @@
+let obj = {
+    "studebtID": 64309010041 ,
+    "name": "Java" ,
+    "lastname": "Script",
+    "geade": 3.45 ,
+    "Tel":[191,1112]
+}
+console.log(obj.name,obj.geade,obj.Tel[0])
